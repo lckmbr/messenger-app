79c4bf2bca9bb8a6f6cc5a647e499b0d515a185f
lckmbr <luckambere@gmail.com>
2025-01-24 23:14:20 +0300

    created tthe register/LogIn Form

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -1,11 +1,17 @@
 "use client";
 
+import Button from "@/app/components/Button";
+import Input from "@/app/components/inputs/Input";
+import AuthSocialButton from "./AuthSocialButton";
+
 import { useCallback, useState } from "react";
-import { FieldValues, useForm, SubmitHandler } from "react-hook-form";
+import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+import { BsGithub, BsGoogle } from "react-icons/bs";
+
 type Variant = "LOGIN" | "REGISTER";
 
 const AuthForm = () => {
-  const [variant, setVariant] = useState<Variant>("LOGIN");
+  const [variant, setVariant] = useState<Variant>("REGISTER");
   const [isLoading, setIsLoading] = useState(false);
 
   const toggleVariant = useCallback(() => {
@@ -19,7 +25,7 @@ const AuthForm = () => {
   const {
     register,
     handleSubmit,
-    formState: errors,
+    formState: { errors },
   } = useForm<FieldValues>({
     defaultValues: {
       name: "",
@@ -36,15 +42,125 @@ const AuthForm = () => {
     }
 
     if (variant === "LOGIN") {
-      //Next Auth SignIn
+      //NextAuth SignIn
     }
   };
 
   const socialAction = (action: string) => {
     setIsLoading(true);
+
+    //NextAuth Social Sign In
   };
 
-  return <div></div>;
+  return (
+    <div
+      className="
+        mt-8
+        sm:mx-auto
+        sm:w-full
+        sm:max-w-md
+      "
+    >
+      <div
+        className="
+        bg-white
+        px-4
+        py-8
+        shadow
+        sm:rounded-lg
+        sm:px-10
+      "
+      >
+        <form
+          className="space-y-6"
+          onSubmit={handleSubmit(onSubmit)}
+        >
+          {variant === "REGISTER" && (
+            <Input
+              id="name"
+              label="Name"
+              placeholder="Enter your name"
+              register={register}
+              errors={errors}
+            />
+          )}
+          <Input
+            label="Email"
+            id="email"
+            placeholder="Enter your email address"
+            register={register}
+            errors={errors}
+          />
+          <Input
+            id="password"
+            label="Password"
+            placeholder="Enter your password"
+            type="password"
+            register={register}
+            errors={errors}
+          />
+          <div>
+            <Button
+              disabled={isLoading}
+              fullWidth
+              type="submit"
+            >
+              {variant === "LOGIN" ? "Sign in" : "Register"}
+            </Button>
+          </div>
+        </form>
+
+        <div className="mt-6">
+          <div className="relative">
+            <div
+              className="
+                absolute
+                inset-0
+                flex
+                items-center
+              "
+            >
+              <div
+                className="
+                  w-full
+                  border-t
+                  border-gray-300
+                "
+              />
+            </div>
+            <div
+              className="
+                  relative
+                  flex
+                  justify-center
+                  text-sm
+                "
+            >
+              <span
+                className="
+                  bg-white
+                  px-2 text-gray-500
+                "
+              >
+                Or continue with
+              </span>
+            </div>
+          </div>
+
+          <div className="mt-6 flex gap-2">
+            <AuthSocialButton
+              icon={BsGithub}
+              onClick={() => socialAction("github")}
+            />
+            <AuthSocialButton
+              icon={BsGoogle}
+              onClick={() => socialAction("google")}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default AuthForm;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import forms from "@tailwindcss/forms";
 
 export default {
   content: [
@@ -9,5 +10,5 @@ export default {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [forms({ strategy: "class" })],
 } satisfies Config;
